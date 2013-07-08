357b0b1188ffedbc5d7fc4135620ee6ce0dd9006
Brady Wied <brady@wied.us>
2013-07-08 00:02:59 -0500

    Clean up test and add received test

--- MsbwUtilities/test/Implementation/MsBwUtilityTest/Net/VariableDelimiterTcpClientTest.cs
+++ MsbwUtilities/test/Implementation/MsBwUtilityTest/Net/VariableDelimiterTcpClientTest.cs
@@ -1,7 +1,6 @@
 ﻿#region
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -21,27 +20,32 @@ namespace MsBwUtilityTest.Net
     public class VariableDelimiterTcpClientTest : BaseTest
     {
         private const int DUMMY_LOCAL_PORT = 5000;
+        private const string TERMINATOR = "THE_END";
+        private const string ERROR = "BIG_ERROR";
+        private const string SCRUB = "NSA";
         private TcpClient _tcpClient;
-        private List<string> _responsesReceived;
         private VariableDelimiterTcpClient _client;
         private CancellationTokenSource _cts;
         private TcpClient _dummyClient;
         private NetworkStream _dummyStream;
         private StreamWriter _dummyWriter;
         private StreamReader _dummyReader;
+        private TaskCompletionSource<string> _taskCompleted;
+
+        #region Setup/Teardown
 
         [SetUp]
         public override void SetUp()
         {
             base.SetUp();
             _tcpClient = new TcpClient();
-            _responsesReceived = new List<string>();
             _client = null;
             _dummyClient = null;
             _dummyStream = null;
             _dummyWriter = null;
             _dummyReader = null;
             _cts = new CancellationTokenSource();
+            _taskCompleted = null;
         }
 
         [TearDown]
@@ -53,6 +57,10 @@ namespace MsBwUtilityTest.Net
             }
             base.TearDown();
         }
+
+        #endregion
+
+        #region Utility Methods
 
         private void ConnectTcpClient()
         {
@@ -92,7 +100,12 @@ namespace MsBwUtilityTest.Net
 
         private void ClientResponseReceived(string response)
         {
-            _responsesReceived.Add(response);
+            _taskCompleted.SetResult(response);
+        }
+
+        private void SetupResponseWait()
+        {
+            _taskCompleted = new TaskCompletionSource<string>();
         }
 
         private void SetupClient()
@@ -101,35 +114,73 @@ namespace MsBwUtilityTest.Net
             Task.Factory.StartNew(() => _client.ResponseLoop(_cts.Token));
         }
 
+
+        private void SetupVariableDelimiterClient()
+        {
+            _client = new VariableDelimiterTcpClient(client: _tcpClient,
+                                                     defaultTerminator: TERMINATOR,
+                                                     haltResponseWaitOnKeyword: ERROR,
+                                                     scrubThisFromLogs: SCRUB);
+        }
+
+        #endregion
+
+        #region Tests
+
         [Test]
-        public void Log_scrub_works_properly()
+        public void Log_scrub_works_properly_on_send()
         {
             // arrange
             StartWritableSocket();
             ConnectTcpClient();
-            _client = new VariableDelimiterTcpClient(client: _tcpClient,
-                                                     defaultTerminator: "THE_END",
-                                                     haltResponseWaitOnKeyword: "BIG_ERROR",
-                                                     scrubThisFromLogs: "NSA");
+            SetupVariableDelimiterClient();
             SetupClient();
+            const string textToSend = "Login with " + SCRUB + " password";
 
             // act
-            _client.Send(new DummyRequest {Flat = "Login with NSA password"});
+            
+            _client.Send(new DummyRequest {Flat = textToSend});
 
             // assert
+            // should still get non-scrubbed request on other side
             var text = _dummyReader.ReadLine();
             text
                 .Should()
-                .Be("Login with NSA password");
+                .Be(textToSend);
 
             TargetForTesting
                 .LogMessages
-                .ShouldBeEquivalentTo(new[]
-                    {
-                        "Sending Login with *** password",
-                        "Beginning response wait",
-                        "Calling stream read"
-                    });
+                .Should()
+                .NotContain(logMsg => logMsg.Contains(SCRUB));
         }
+
+        [Test]
+        public async Task Log_scrub_works_properly_on_receive()
+        {
+            // arrange
+            StartWritableSocket();
+            ConnectTcpClient();
+            SetupVariableDelimiterClient();
+            SetupClient();
+            const string textToReceive = "Login with " + SCRUB + " password " + TERMINATOR;
+
+            // act
+            SetupResponseWait();
+            _dummyWriter.Write(textToReceive);
+
+            // assert
+            var response = await _taskCompleted.Task;
+            // should still get the correct text
+            response
+                .Should()
+                .Be(textToReceive);
+
+            TargetForTesting
+                .LogMessages
+                .Should()
+                .NotContain(logMsg => logMsg.Contains(SCRUB));
+        }
+
+        #endregion
     }
 }
