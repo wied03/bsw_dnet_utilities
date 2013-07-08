28ffb6b4307c6bcc550665f5f773e7de4eb36ee6
Brady Wied <brady@wied.us>
2013-07-07 22:56:16 -0500

    Added more tests for cancel cases

--- MsbwUtilities/test/Implementation/MsBwUtilityTest/Tasks/TaskExtensionsTest.cs
+++ MsbwUtilities/test/Implementation/MsBwUtilityTest/Tasks/TaskExtensionsTest.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MsBw.MsBwUtility.Tasks;
@@ -47,6 +48,63 @@ namespace MsBwUtilityTest.Tasks
                 .ShouldThrow<AggregateException>()
                 .WithInnerException<TimeoutException>()
                 ;
+        }
+
+        [Test]
+        public async Task Timeout_or_cancel_normal()
+        {
+            // arrange
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var task = DelayFor(500.Milliseconds());
+
+            // act
+            var result = await task.TimeoutOrCancel(token,
+                                                    1.Seconds());
+
+            // assert
+            result
+                .Should()
+                .Be(Result.Normal);
+        }
+
+        [Test]
+        public async Task Timeout_or_cancel_timed_out()
+        {
+            // arrange
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var task = DelayFor(1.Seconds());
+
+            // act
+            var result = await task.TimeoutOrCancel(token,
+                                                    200.Milliseconds());
+
+            // assert
+            result
+                .Should()
+                .Be(Result.Timeout);
+        }
+
+        [Test]
+        public async Task Timeout_or_cancel_canceled()
+        {
+            // arrange
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var task = DelayFor(10.Seconds());
+
+            // act
+            var resultTask = task.TimeoutOrCancel(token,
+                                                  20.Seconds());
+            await Task.Delay(100.Milliseconds());
+            await Task.Factory.StartNew(cts.Cancel);
+            var result = await resultTask;
+
+            // assert
+            result
+                .Should()
+                .Be(Result.Canceled);
         }
     }
 }
