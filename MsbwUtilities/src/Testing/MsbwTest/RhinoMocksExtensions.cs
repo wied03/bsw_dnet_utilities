e4e3c2871d025105186ee60ea74ece1e0d4dfa95
Brady Wied <brady@wied.us>
2013-09-28 12:05:45 -0500

    Added Rhino mocks convenience method

---
+++ MsbwUtilities/src/Testing/MsbwTest/RhinoMocksExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright 2013 BSW Technology Consulting, released under the BSD license - see LICENSING.txt at the top of this repository for details
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using Castle.Core.Interceptor;
+using Rhino.Mocks;
+using Rhino.Mocks.Interfaces;
+
+namespace MsbwTest
+{
+    public static class RhinoMocksExtensions
+    {
+        public static void SetupPropertyBehaviorOnAll(this object mock)
+        {
+            var mockedInterface = mock.MockedInterface();
+            var props = mockedInterface.GetProperties();
+            props.Where(p => p.CanRead && p.CanWrite).ToList().ForEach(p => mock.Stub(o => p.GetValue(o))
+                                                                                .PropertyBehavior());
+        }
+
+        public static Type MockedInterface(this object mock)
+        {
+            return mock.GetType()
+                       .GetInterfaces()
+                       .First(it => it != typeof (IMockedObject) &&
+                                    it != typeof (ISerializable) &&
+                                    it != typeof (IProxyTargetAccessor));
+        }
+    }
+}
---
+++ MsbwUtilities/test/Testing/MsbwTest_Test/RhinoMocksExtensionsTest.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2013 BSW Technology Consulting, released under the BSD license - see LICENSING.txt at the top of this repository for details
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using MsbwTest;
+using NUnit.Framework;
+using FluentAssertions;
+using Rhino.Mocks;
+
+namespace MsbwTest_Test
+{
+    public interface ITest
+    {
+        string Prop1 { get; set; }
+        int Prop2 { get; set; }
+    }
+
+    [TestFixture]
+    public class RhinoMocksExtensionsTest : BaseTest
+    {
+        #region Setup/Teardown
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+        }
+
+        #endregion
+
+        #region Utility Methods
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Setup_property_behavior_on_all()
+        {
+            // arrange
+            var mock = MockRepository.GenerateMock<ITest>();
+
+            // act
+            mock.SetupPropertyBehaviorOnAll();
+            mock.Prop1 = "foo";
+            mock.Prop2 = 2003;
+
+            // assert
+            mock.Prop1
+                .Should()
+                .Be("foo");
+            mock.Prop2
+                .Should()
+                .Be(2003);
+        }
+
+        #endregion
+    }
+}
