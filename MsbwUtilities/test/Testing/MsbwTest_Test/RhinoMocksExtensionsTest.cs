c5912aba788b826adab33a5add4f8de05600423e
Brady Wied - Win <brady@wied.us>
2013-09-28 12:59:05 -0500

    Added exception for stubs that we don't want to impl property behavior for

--- MsbwUtilities/test/Testing/MsbwTest_Test/RhinoMocksExtensionsTest.cs
+++ MsbwUtilities/test/Testing/MsbwTest_Test/RhinoMocksExtensionsTest.cs
@@ -14,6 +14,7 @@ namespace MsbwTest_Test
     {
         string Prop1 { get; set; }
         int Prop2 { get; set; }
+        string Prop3 { get; set; }
     }
 
     [TestFixture]
@@ -53,6 +54,33 @@ namespace MsbwTest_Test
             mock.Prop2
                 .Should()
                 .Be(2003);
+        }
+
+        [Test]
+        public void Setup_property_behavior_on_all_except()
+        {
+            // arrange
+            var mock = MockRepository.GenerateMock<ITest>();
+
+            // act
+            mock.SetupPropertyBehaviorOnAllExcept(c => c.Prop1,
+                                                  c => c.Prop3);
+            mock.Prop2 = 9999;
+            mock.Stub(c => c.Prop1)
+                .Return("foobar");
+            mock.Stub(c => c.Prop3)
+                .Return("bar");
+
+            // assert
+            mock.Prop1
+                .Should()
+                .Be("foobar");
+            mock.Prop2
+                .Should()
+                .Be(9999);
+            mock.Prop3
+                .Should()
+                .Be("bar");
         }
 
         #endregion
