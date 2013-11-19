bd7341cd0028a01c13fbbe3ad63b99bf374f01c0
Brady Wied <brady@wied.us>
2013-11-18 20:51:17 -0600

    Simplify class into encryption helper and use in composition inside .net config

---
+++ MsbwUtilities/src/Implementation/MsBwUtility/Config/ICryptoHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace MsBw.MsBwUtility.Config
+{
+    public interface ICryptoHelper
+    {
+        string Encrypt(byte[] redBytes);
+        int? DecryptNullableInt(string base64);
+        string Encrypt(int? value);
+        string Decrypt(string base64);
+        string Encrypt(string value);
+        byte[] DecryptAsBytes(string blackBase64);
+    }
+}
---
+++ MsbwUtilities/test/Implementation/MsBwUtilityTest/Config/CryptoHelperTest.cs
@@ -0,0 +1,117 @@
+﻿// Copyright 2013 BSW Technology Consulting, released under the BSD license - see LICENSING.txt at the top of this repository for details
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using FluentAssertions;
+using MsBw.MsBwUtility;
+using MsBw.MsBwUtility.Config;
+using NUnit.Framework;
+
+namespace MsBwUtilityTest.Config
+{
+    [TestFixture]
+    public class CryptoHelperTest : BaseTest
+    {
+        private ICryptoHelper _crypto;
+
+        #region Setup/Teardown
+
+        [SetUp]
+        public override void SetUp()
+        {
+            base.SetUp();
+            _crypto = new CryptoHelper();
+        }
+
+        #endregion
+
+        #region Utility Methods
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Encrypt_red_bytes()
+        {
+            // arrange
+            var bytes = "AB".ToByteArrayFromHex();
+
+            // act
+            var encrypted = _crypto.Encrypt(bytes);
+            var decrypted = _crypto.DecryptAsBytes(encrypted);
+
+            // assert
+            var asHex = decrypted.ToHex();
+            asHex
+                .Should()
+                .Be("AB");
+            encrypted
+                .Should()
+                .NotBe("AB",
+                       "Crypto?");
+        }
+
+        [Test]
+        public void Encrypt_nullable_int_has_value()
+        {
+            // arrange
+            int? stuff = 2;
+
+            // act
+            var encrypted = _crypto.Encrypt(stuff);
+            var decrypted = _crypto.DecryptNullableInt(encrypted);
+
+            // assert
+            decrypted
+                .Should()
+                .Be(2);
+            encrypted
+                .Should()
+                .NotBe("2");
+        }
+
+        [Test]
+        public void Encrypt_nullable_int_no_value()
+        {
+            // arrange
+            int? stuff = null;
+
+            // act
+// ReSharper disable once ExpressionIsAlwaysNull
+            var encrypted = _crypto.Encrypt(stuff);
+            var decrypted = _crypto.DecryptNullableInt(encrypted);
+
+            // assert
+            decrypted
+                .Should()
+                .NotHaveValue();
+            encrypted
+                .Should()
+                .BeNull();
+        }
+
+        [Test]
+        public void Encrypt_string()
+        {
+            // arrange
+            const string text = "AB";
+
+            // act
+            var encrypted = _crypto.Encrypt(text);
+            var decrypted = _crypto.Decrypt(encrypted);
+
+            // assert
+            decrypted
+                .Should()
+                .Be("AB");
+            encrypted
+                .Should()
+                .NotBe("AB",
+                       "Crypto?");
+        }
+
+        #endregion
+    }
+}
