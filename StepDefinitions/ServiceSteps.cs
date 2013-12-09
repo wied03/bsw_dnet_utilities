bad907620c260945befe22a8824c9dc777546ea4
Brady Wied <brady@bswtechconsulting.com>
2013-12-09 11:56:14 -0600

    Improve service cleanup code (ServiceController does not update state with each call to Status) and add test to ensure buttons are disabled

--- StepDefinitions/ServiceSteps.cs
+++ StepDefinitions/ServiceSteps.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright 2013 BSW Technology Consulting, released under the BSD license - see LICENSING.txt at the top of this repository for details
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.ServiceProcess;
@@ -13,35 +14,43 @@ namespace Bsw.Utilities.Windows.SystemTest.StepDefinitions
     [Binding]
     public class ServiceSteps : BaseSteps<GeneralScenarioContext>
     {
+        [BeforeScenario]
+        public static void ClearServices()
+        {
+            ContextStatic.ServicesStarted = new List<string>();
+        }
+
         [When(@"I start the '(.*)' service")]
         public void WhenIStartTheService(string serviceName)
         {
             var controller = new ServiceController(serviceName);
-            Context.ServiceController = controller;
             controller.Status
                       .Should()
                       .Be(ServiceControllerStatus.Stopped,
                           "Can't start a service that isn't stopped");
             controller.Start();
+            Context.ServicesStarted.Add(serviceName);
         }
 
         [AfterScenario]
         public static void ShutdownOrphanedService()
         {
-            var controller = ContextStatic.ServiceController;
-            if (controller == null || controller.Status != ServiceControllerStatus.Running) return;
-            Console.WriteLine("Service was orphaned in running state, stopping");
-            controller.Stop();
+            // ServiceController doesn't update Status once constructed, so need to reconstruct each time
+            var runningServices = ContextStatic.ServicesStarted.Select(serviceName => new ServiceController(serviceName))
+                                               .Where(controller => controller.Status == ServiceControllerStatus.Running);
+            foreach (var controller in runningServices)
+            {
+                Console.WriteLine("Service was orphaned in running state, stopping");
+                controller.Stop();
+            }
         }
 
         [When(@"I stop the '(.*)' service")]
         public void WhenIStopTheService(string serviceName)
         {
-            var existingController = Context.ServiceController;
-            var controllerToUse = existingController != null && existingController.ServiceName == serviceName
-                                      ? existingController
-                                      : new ServiceController(serviceName);
-            controllerToUse.Stop();
+            // ServiceController doesn't update Status once constructed, so need to reconstruct each time
+            var controller = new ServiceController(serviceName);
+            controller.Stop();
         }
     }
 }
