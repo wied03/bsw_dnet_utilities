f7e45189707a2e77e199335abc457b66b635833e
Brady Wied <brady@bswtechconsulting.com>
2014-01-16 13:35:14 -0600

    Log fixture level stuff as well (and test it)

--- MsbwUtilities/src/Testing/Bsw.NUnit.Traceability.Addin/JiraTraceability.cs
+++ MsbwUtilities/src/Testing/Bsw.NUnit.Traceability.Addin/JiraTraceability.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright 2014 BSW Technology Consulting, released under the BSD license - see LICENSING.txt at the top of this repository for details
 
 using System;
-using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -18,7 +18,8 @@ namespace Bsw.NUnit.Traceability.Addin
                                     EventListener
     {
         private static readonly Regex JiraRegex = new Regex(@"JIRA_(\w+_\d+)");
-        private readonly IDictionary<string, Type> _testTypeMapping;
+        private readonly IDictionary<string, Type> _typeNameToReflectedTypeCache;
+        private readonly IDictionary<Type, string> _alreadyWrittenFixtureLevelCategories;
         private readonly IWriteCategoriesToOutput _writer;
 
         public JiraTraceability() : this(new WriteCategoriesToOutput())
@@ -28,7 +29,8 @@ namespace Bsw.NUnit.Traceability.Addin
         internal JiraTraceability(IWriteCategoriesToOutput writer)
         {
             _writer = writer;
-            _testTypeMapping = new Dictionary<string, Type>();
+            _typeNameToReflectedTypeCache = new Dictionary<string, Type>();
+            _alreadyWrittenFixtureLevelCategories = new Dictionary<Type, string>();
         }
 
         public bool Install(IExtensionHost host)
@@ -54,11 +56,28 @@ namespace Bsw.NUnit.Traceability.Addin
         public void TestStarted(TestName testName)
         {
             var method = GetMethodInfo(testName);
-            var categories = method.GetCustomAttributes(typeof (CategoryAttribute),
-                                                        true)
-                                   .Cast<CategoryAttribute>()
-                                   .Select(c => c.Name)
-                                   .ToList()
+            var declaringType = method.DeclaringType;
+            // only do fixture level stuff once since we can only intercept each test event
+            if (!_alreadyWrittenFixtureLevelCategories.ContainsKey(declaringType))
+            {
+                Debug.Assert(declaringType != null,
+                             "declaringType != null");
+                var fixtureAttributes = declaringType.GetCustomAttributes(typeof (CategoryAttribute),
+                                                                          true);
+                WriteCategories(fixtureAttributes);
+                _alreadyWrittenFixtureLevelCategories[declaringType] = string.Empty;
+            }
+            var attributes = method.GetCustomAttributes(typeof (CategoryAttribute),
+                                                        true);
+            WriteCategories(attributes);
+        }
+
+        private void WriteCategories(IEnumerable<object> categoryAttributes)
+        {
+            var categories = categoryAttributes
+                .Cast<CategoryAttribute>()
+                .Select(c => c.Name)
+                .ToList()
                 ;
             if (!categories.Any()) return;
             foreach (var category in categories)
@@ -85,9 +104,9 @@ namespace Bsw.NUnit.Traceability.Addin
             var marker = fullName.LastIndexOf('.');
             var type = fullName.Substring(0,
                                           marker);
-            var theType = _testTypeMapping.ContainsKey(type)
-                              ? _testTypeMapping[type]
-                              : (_testTypeMapping[type] = GetTestType(type));
+            var theType = _typeNameToReflectedTypeCache.ContainsKey(type)
+                              ? _typeNameToReflectedTypeCache[type]
+                              : (_typeNameToReflectedTypeCache[type] = GetTestType(type));
             var testMethod = fullName.Substring(marker + 1);
             var method = theType.GetMethod(testMethod);
             return method;
---
+++ MsbwUtilities/test/Testing/Bsw.NUnit.Traceability.Addin.Test/JiraTraceabilityTestSomeCat.cs
@@ -0,0 +1,268 @@
+﻿// Copyright 2014 BSW Technology Consulting, released under the BSD license - see LICENSING.txt at the top of this repository for details
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using FluentAssertions;
+using NUnit.Core;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Bsw.NUnit.Traceability.Addin.Test
+{
+    public abstract class BaseJiraTest
+    {
+        protected JiraTraceability Addin { get; private set; }
+        protected List<string> Written { get; private set; }
+
+        #region Setup/Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            var writer = MockRepository.GenerateMock<IWriteCategoriesToOutput>();
+            Addin = new JiraTraceability(writer);
+            Written = new List<string>();
+            writer.Stub(w => w.Write(null))
+                  .IgnoreArguments()
+                  .Do(new Action<string>(txt => Written.Add(txt)));
+        }
+
+        #endregion
+
+        #region Utility Methods
+
+        protected static TestName TestNameBasedOnCurrentTestMethod
+        {
+            get
+            {
+                // ReSharper disable PossibleNullReferenceException
+                var frame = new StackTrace().GetFrames()[1];
+                var method = frame.GetMethod();
+                var theType = method.DeclaringType.FullName;
+                // ReSharper restore PossibleNullReferenceException
+                var methodName = method.Name;
+                return new TestName
+                {
+                    FullName = string.Format("{0}.{1}",
+                                             theType,
+                                             methodName)
+                };
+            }
+        }
+
+        #endregion
+    }
+
+    [TestFixture]
+    public class JiraTraceabilityTestNoCat : BaseJiraTest
+    {
+        [Test]
+        public void No_cat()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .Should()
+                .BeEmpty();
+        }
+
+        [Test]
+        [Category("some other stuff")]
+        public void Other_cat()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[] { "Test Category: some other stuff" });
+        }
+
+        [Test]
+        [Category("JIRA_CRT_123")]
+        public void Jira_cat()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[] { "Related JIRA Issue: CRT-123" });
+        }
+    }
+
+    [TestFixture]
+    [Category("JIRA_CRT_123")]
+    public class JiraTraceabilityTestJiraCat : BaseJiraTest
+    {
+        [Test]
+        public void No_cat()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[] {"Related JIRA Issue: CRT-123"});
+        }
+
+        [Test]
+        [Category("some other stuff")]
+        public void Other_cat()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[] {"Related JIRA Issue: CRT-123", "Test Category: some other stuff"});
+        }
+
+        [Test]
+        [Category("JIRA_CRT_123")]
+        public void Jira_cat()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[] {"Related JIRA Issue: CRT-123", "Related JIRA Issue: CRT-123"});
+        }
+    }
+
+    [TestFixture]
+    [Category("fixture level cat")]
+    public class JiraTraceabilityTestSomeCat : BaseJiraTest
+    {
+        #region Tests
+
+        [Test]
+        [Category("some other stuff")]
+        public void Non_jira_category()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[] { "Test Category: fixture level cat", "Test Category: some other stuff" });
+        }
+
+        [Test]
+        [Category("some other stuff")]
+        [Category("some other stuff 2")]
+        public void Non_jira_categories()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[]
+                                      {
+                                          "Test Category: fixture level cat", 
+                                          "Test Category: some other stuff",
+                                          "Test Category: some other stuff 2"
+                                      });
+        }
+
+        [Test]
+        public void No_categories()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                 .ShouldBeEquivalentTo(new[] { "Test Category: fixture level cat" });
+        }
+
+        [Test]
+        public void Only_log_fixture_level_categories_once()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .Should()
+                .HaveCount(1,
+                           "No categories on test method, should only do the type level stuff once");
+        }
+
+        [Test]
+        [Category("JIRA_CRT_456")]
+        [Category("JIRA_CRT_123")]
+        public void Multiple_jira_categories()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[]
+                                      {
+                                          "Test Category: fixture level cat",
+                                          "Related JIRA Issue: CRT-123",
+                                          "Related JIRA Issue: CRT-456"
+                                      });
+        }
+
+        [Test]
+        [Category("JIRA_CRT_123")]
+        public void Single_jira_category()
+        {
+            // arrange
+            var testName = TestNameBasedOnCurrentTestMethod;
+
+            // act
+            Addin.TestStarted(testName);
+
+            // assert
+            Written
+                .ShouldBeEquivalentTo(new[] { "Test Category: fixture level cat", "Related JIRA Issue: CRT-123" });
+        }
+
+        #endregion
+    }
+}
